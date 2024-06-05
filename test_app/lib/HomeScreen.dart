c469fc9a4b3a3550b4b807fa973e59bf1e4d67f5
Roshan pcy <godxrosu@gmail.com>
2024-06-05 10:34:17 +0530

    added

---
+++ test_app/lib/HomeScreen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/counter.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Counter counter = Counter();
+
+  void _incrementCounter() {
+    setState(() {
+      counter.increment();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${counter.count}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ test_app/lib/counter.dart
@@ -0,0 +1,6 @@
+class Counter {
+  int count = 0;
+  void increment() {
+    count = count + 1;
+  }
+}
